e5d42b16ec7bc0c51f33cb1afb97a37b7097ff17
NotKili <familie_kessler.kilian@t-online.de>
2023-12-01 09:45:24 +0100

    feat(input): DoubleInput operations

--- src/dev/notkili/aoc/shared/input/DoubleInput.java
+++ src/dev/notkili/aoc/shared/input/DoubleInput.java
@@ -3,6 +3,9 @@ package dev.notkili.aoc.shared.input;
 import dev.notkili.aoc.shared.Solution;
 
 import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class DoubleInput implements Input<DoubleInput> {
     private final double value;
@@ -35,6 +38,90 @@ public class DoubleInput implements Input<DoubleInput> {
         return value;
     }
 
+    public StringInput asStringInput() {
+        return new StringInput(value + "");
+    }
+
+    public IntInput asIntInput() {
+        return new IntInput((int) value);
+    }
+
+    public LongInput asLongInput() {
+        return new LongInput((long) value);
+    }
+
+    public DoubleInput add(DoubleInput other) {
+        return new DoubleInput(value + other.value);
+    }
+
+    public DoubleInput add(double x) {
+        return new DoubleInput(value + x);
+    }
+
+    public DoubleInput subtract(DoubleInput other) {
+        return new DoubleInput(value - other.value);
+    }
+
+    public DoubleInput subtract(double x) {
+        return new DoubleInput(value - x);
+    }
+
+    public DoubleInput multiply(DoubleInput other) {
+        return new DoubleInput(value * other.value);
+    }
+
+    public DoubleInput multiply(double x) {
+        return new DoubleInput(value * x);
+    }
+
+    public DoubleInput divide(DoubleInput other) {
+        return new DoubleInput(value / other.value);
+    }
+
+    public DoubleInput divide(double x) {
+        return new DoubleInput(value / x);
+    }
+
+    public DoubleInput aggregateUntil(BiFunction<DoubleInput, DoubleInput, DoubleInput> aggregator, Function<DoubleInput, DoubleInput> translator, Predicate<DoubleInput> until) {
+        DoubleInput toReturn = new DoubleInput(0.0d);
+        DoubleInput current = this;
+
+        while (!until.test(current)) {
+            current = translator.apply(current);
+            toReturn = aggregator.apply(toReturn, current);
+        }
+
+        return toReturn;
+    }
+
+    public LongInput floor() {
+        return new LongInput((long) Math.floor(value));
+    }
+
+    public LongInput ceil() {
+        return new LongInput((long) Math.ceil(value));
+    }
+
+    public LongInput round() {
+        return new LongInput(Math.round(value));
+    }
+
+    public DoubleInput abs() {
+        return new DoubleInput(Math.abs(value));
+    }
+
+    public DoubleInput sqrt() {
+        return new DoubleInput(Math.sqrt(value));
+    }
+
+    public DoubleInput pow(DoubleInput other) {
+        return new DoubleInput(Math.pow(value, other.value));
+    }
+
+    public DoubleInput pow(double x) {
+        return new DoubleInput(Math.pow(value, x));
+    }
+
     @Override
     public int compareTo(DoubleInput o) {
         return Double.compare(value, o.value);
@@ -61,18 +148,6 @@ public class DoubleInput implements Input<DoubleInput> {
     @Override
     public void print() {
         System.out.print(value);
-    }
-
-    public StringInput asStringInput() {
-        return new StringInput(value + "");
-    }
-
-    public IntInput asIntInput() {
-        return new IntInput((int) value);
-    }
-
-    public LongInput asLongInput() {
-        return new LongInput((long) value);
     }
 
     @Override
