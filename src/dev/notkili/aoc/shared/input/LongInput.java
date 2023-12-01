fda5c66464050d4b73ed0a59e8954a279f126742
NotKili <familie_kessler.kilian@t-online.de>
2023-12-01 09:47:04 +0100

    feat(input): LongInput operations

--- src/dev/notkili/aoc/shared/input/LongInput.java
+++ src/dev/notkili/aoc/shared/input/LongInput.java
@@ -3,6 +3,9 @@ package dev.notkili.aoc.shared.input;
 import dev.notkili.aoc.shared.Solution;
 
 import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class LongInput implements Input<LongInput> {
     private final long value;
@@ -31,6 +34,86 @@ public class LongInput implements Input<LongInput> {
         return value;
     }
 
+    public StringInput asStringInput() {
+        return new StringInput(value + "");
+    }
+
+    public IntInput asIntInput() {
+        return new IntInput((int) value);
+    }
+
+    public DoubleInput asDoubleInput() {
+        return new DoubleInput(value);
+    }
+
+    public LongInput add(LongInput other) {
+        return new LongInput(value + other.value);
+    }
+
+    public LongInput add(long x) {
+        return new LongInput(value + x);
+    }
+
+    public LongInput subtract(LongInput other) {
+        return new LongInput(value - other.value);
+    }
+
+    public LongInput subtract(long x) {
+        return new LongInput(value - x);
+    }
+
+    public LongInput multiply(LongInput other) {
+        return new LongInput(value * other.value);
+    }
+
+    public LongInput multiply(long x) {
+        return new LongInput(value * x);
+    }
+
+    public LongInput divide(LongInput other) {
+        return new LongInput(value / other.value);
+    }
+
+    public LongInput divide(long x) {
+        return new LongInput(value / x);
+    }
+
+    public LongInput pow(LongInput other) {
+        return new LongInput((long) Math.pow(value, other.value));
+    }
+
+    public LongInput pow(long x) {
+        return new LongInput((long) Math.pow(value, x));
+    }
+
+    public LongInput mod(LongInput other) {
+        return new LongInput(value % other.value);
+    }
+
+    public LongInput mod(long x) {
+        return new LongInput(value % x);
+    }
+
+    public LongInput abs() {
+        return new LongInput(Math.abs(value));
+    }
+
+    public LongInput negate() {
+        return new LongInput(-value);
+    }
+
+    public LongInput aggregateUntil(BiFunction<LongInput, LongInput, LongInput> aggregator, Function<LongInput, LongInput> translator, Predicate<LongInput> until) {
+        LongInput toReturn = new LongInput(0L);
+        LongInput current = this;
+
+        while (!until.test(current)) {
+            current = translator.apply(current);
+            toReturn = aggregator.apply(toReturn, current);
+        }
+
+        return toReturn;
+    }
+
     @Override
     public int compareTo(LongInput o) {
         return Long.compare(value, o.value);
@@ -57,18 +140,6 @@ public class LongInput implements Input<LongInput> {
     @Override
     public void print() {
         System.out.print(value);
-    }
-
-    public StringInput asStringInput() {
-        return new StringInput(value + "");
-    }
-
-    public IntInput asIntInput() {
-        return new IntInput((int) value);
-    }
-
-    public DoubleInput asDoubleInput() {
-        return new DoubleInput(value);
     }
 
     @Override
