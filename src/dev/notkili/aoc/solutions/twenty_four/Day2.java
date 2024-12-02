be34b8af1d35ba94092ae9e00fd617ebad940dd3
NotKili <familie_kessler.kilian@t-online.de>
2024-12-02 06:53:46 +0100

    feat(2024): Day 2

---
+++ src/dev/notkili/aoc/solutions/twenty_four/Day2.java
@@ -0,0 +1,159 @@
+package dev.notkili.aoc.solutions.twenty_four;
+
+import dev.notkili.aoc.shared.input.IntInput;
+import dev.notkili.aoc.shared.input.StringInput;
+import dev.notkili.aoc.shared.parse.InputParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.println("Solution 1:");
+        
+        new InputParser(2024, 2).getInput().ifPresent(strInput -> {
+            strInput.splitAt("\n").mapTo(str -> str.splitAt(" ")).mapTo(report -> report.mapTo(StringInput::asInt).asList()).mapTo(Day2::safe).mapTo(b -> b ? 1 : 0).reduce(Integer::sum).ifPresent(System.out::println);
+        });
+        
+        System.out.println("\nSolution 2:");
+        
+        new InputParser(2024, 2).getInput().ifPresent(strInput -> {
+            strInput.splitAt("\n").mapTo(str -> str.splitAt(" ")).mapTo(report -> report.mapTo(StringInput::asInt).asList()).mapTo(report -> safeRem(report)).mapTo(b -> b ? 1 : 0).reduce(Integer::sum).ifPresent(System.out::println);
+        });
+    }
+    
+    private static boolean safe(List<IntInput> in) {
+        if (in.size() == 1) {
+            return true;
+        }
+        
+        var dir = in.get(0).asInt() < in.get(1).asInt();
+        
+        for (int i = 0; i < in.size() - 1; i++) {
+            if (dir) {
+                if (in.get(i).asInt() >= in.get(i + 1).asInt()) {
+                    return false;
+                }
+                
+                if (in.get(i + 1).asInt() - in.get(i).asInt() > 3) {
+                    return false;
+                }
+            } else {
+                if (in.get(i).asInt() <= in.get(i + 1).asInt()) {
+                    return false;
+                }
+                
+                if (in.get(i).asInt() - in.get(i + 1).asInt() > 3) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+
+    private static boolean dir(List<IntInput> in) {
+        var a = 0;
+        var b = 0;
+        
+        for (int i = 0; i < in.size() - 1; i++) {
+            if (in.get(i).asInt() < in.get(i + 1).asInt()) {
+                a++;
+            } else {
+                b++;
+            }
+        }
+        
+        return a > b;
+    }
+    
+    private static boolean safeRem(List<IntInput> in) {
+        if (safe(in)) {
+            return true;
+        }
+
+        var dir = dir(in);
+        
+        for (int i = 0; i < in.size() - 1; i++) {
+            if (dir) {
+                if (in.get(i).asInt() >= in.get(i + 1).asInt()) {
+                    var cpyA = new ArrayList<>(in);
+                    var cpyB = new ArrayList<>(in);
+                    
+                    cpyA.remove(i);
+                    
+                    if (safe(cpyA)) {
+                        return true;
+                    }
+                    
+                    cpyB.remove(i + 1);
+                    
+                    if (safe(cpyB)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+
+                if (in.get(i + 1).asInt() - in.get(i).asInt() > 3) {
+                    var cpyA = new ArrayList<>(in);
+                    var cpyB = new ArrayList<>(in);
+
+                    cpyA.remove(i);
+
+                    if (safe(cpyA)) {
+                        return true;
+                    }
+
+                    cpyB.remove(i + 1);
+
+                    if (safe(cpyB)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                if (in.get(i).asInt() <= in.get(i + 1).asInt()) {
+                    var cpyA = new ArrayList<>(in);
+                    var cpyB = new ArrayList<>(in);
+
+                    cpyA.remove(i);
+
+                    if (safe(cpyA)) {
+                        return true;
+                    }
+
+                    cpyB.remove(i + 1);
+
+                    if (safe(cpyB)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+
+                if (in.get(i).asInt() - in.get(i + 1).asInt() > 3) {
+                    var cpyA = new ArrayList<>(in);
+                    var cpyB = new ArrayList<>(in);
+
+                    cpyA.remove(i);
+
+                    if (safe(cpyA)) {
+                        return true;
+                    }
+
+                    cpyB.remove(i + 1);
+
+                    if (safe(cpyB)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        throw new RuntimeException("Unreachable");
+    }
+}
