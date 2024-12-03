108b3f70453863604c054a1c94c81b1555e8a28b
NotKili <familie_kessler.kilian@t-online.de>
2024-12-03 07:00:14 +0100

    refactor(2024): cleaned up part 3 after previous refactoring changes

--- src/dev/notkili/aoc/solutions/twenty_four/Day3.java
+++ src/dev/notkili/aoc/solutions/twenty_four/Day3.java
@@ -1,9 +1,11 @@
 package dev.notkili.aoc.solutions.twenty_four;
 
 import dev.notkili.aoc.shared.Pattern;
+import dev.notkili.aoc.shared.input.LongInput;
 import dev.notkili.aoc.shared.input.StringInput;
 import dev.notkili.aoc.shared.parse.InputParser;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 
 public class Day3 {
@@ -11,42 +13,48 @@ public class Day3 {
         System.out.println("Solution 1:");
 
         new InputParser(2024, 3).getInput().ifPresent(strInput -> {
-            Matcher matcher = new Pattern("mul\\(\\d+,\\d+\\)").compile().matcher(strInput.str());
-            
-            Long sum = 0L;
-            
-            while (matcher.find()) {
-                var inp = new StringInput(matcher.group());
-                sum += inp.firstNumber(0).asLong() * inp.firstNumber(1).asLong();
-            }
-            
-            System.out.println(sum);
+            strInput
+                    .all(Pattern.of("mul\\(\\d+,\\d+\\)"))
+                    .mapTo(str -> {
+                        return str.firstNumber(0).asLong() * str.firstNumber(1).asLong();
+                    })
+                    .reduce(Long::sum)
+                    .map(LongInput::new)
+                    .orElseThrow()
+                    .solution()
+                    .print();
         });
 
         System.out.println("\nSolution 2:");
 
         new InputParser(2024, 3).getInput().ifPresent(strInput -> {
-            Matcher matcher = new Pattern("(don't\\(\\))|(do\\(\\))|(mul\\(\\d+,\\d+\\))").compile().matcher(strInput.str());
+            var list = strInput
+                    .all(
+                            Pattern.builder()
+                                    .then("mul\\(\\d+,\\d+\\)")
+                                    .or("don't\\(\\)")
+                                    .or("do\\(\\)")
+                                    .build()
+                    );
             
-            var sum = 0L;
+            var sum = new LongInput(0);
             var enable = true;
             
-            while (matcher.find()) {
-                var inp = new StringInput(matcher.group());
-                if (inp.startsWith("don't()")) {
+            for (var s : list) {
+                if (s.equals("don't()")) {
                     enable = false;
-                } else if (inp.startsWith("do()")) {
+                } else if (s.equals("do()")) {
                     enable = true;
                 } else {
                     if (!enable) {
                         continue;
                     }
                     
-                    sum += inp.firstNumber(0).asLong() * inp.firstNumber(1).asLong();
+                    sum = sum.add(s.firstNumber(0).asLong() * s.firstNumber(1).asLong());
                 }
             }
             
-            System.out.println(sum);
+            sum.solution().print();
         });
     }
 }
