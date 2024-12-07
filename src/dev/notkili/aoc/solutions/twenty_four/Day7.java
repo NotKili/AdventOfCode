fc3ae0db270185e194b3df013e89e9bcc6e108bd
NotKili <familie_kessler.kilian@t-online.de>
2024-12-07 07:53:10 +0100

    feat(2024): cleaned up Day7 using new itertools class

--- src/dev/notkili/aoc/solutions/twenty_four/Day7.java
+++ src/dev/notkili/aoc/solutions/twenty_four/Day7.java
@@ -3,113 +3,97 @@ package dev.notkili.aoc.solutions.twenty_four;
 import dev.notkili.aoc.shared.input.IntInput;
 import dev.notkili.aoc.shared.input.ListInput;
 import dev.notkili.aoc.shared.input.LongInput;
-import dev.notkili.aoc.shared.input.StringInput;
-import dev.notkili.aoc.shared.misc.tuple.Tuple;
+import dev.notkili.aoc.shared.misc.TimeTracker;
+import dev.notkili.aoc.shared.misc.collections.IterTools;
+import dev.notkili.aoc.shared.misc.collections.list.List;
 import dev.notkili.aoc.shared.parse.InputParser;
 
 public class Day7 {
     public static void main(String[] args) {
         System.out.println("Solution 1:");
 
-        new InputParser(2024, 7).getInput().ifPresent(strInput -> {
-            var lines = strInput.split("\n");
-            
-            var tuples = lines.mapTo(t -> t.tuple(": "));
-            
-            var tuples2 = tuples.mapTo(t -> new Tuple<>(t.getA(), t.getB().split(" ")));
-            
-            tuples2.mapTo(t -> {
-                if (isRightEquation(t.getA().longInteger(), t.getB().toInts())) {
-                    return t.getA().longInteger();
-                }
-                
-                return IntInput.zero().asLongInput();
-            }).reduce(LongInput::add).get().solution().print();
+        TimeTracker.trackTime(() -> {
+            new InputParser(2024, 7).getInput().ifPresent(strInput -> {
+                var input = strInput
+                        .split("\n")
+                        .mapTo(t -> t.tuple(": "))
+                        .mapTo(t -> t.mapB(b -> b.split(" ")));
+
+                input.mapTo(t -> {
+                    if (isRightEquation(t.getA().longInteger(), t.getB().toInts())) {
+                        return t.getA().longInteger();
+                    }
+
+                    return IntInput.zero().asLongInput();
+                }).reduce(LongInput::add).get().solution().print();
+            });
         });
-
+        
         System.out.println("Solution 2:");
+        
+        TimeTracker.trackTime(() -> {
+            new InputParser(2024, 7).getInput().ifPresent(strInput -> {
+                var input = strInput
+                        .split("\n")
+                        .mapTo(t -> t.tuple(": "))
+                        .mapTo(t -> t.mapB(b -> b.split(" ")));
 
-        new InputParser(2024, 7).getInput().ifPresent(strInput -> {
-            var lines = strInput.split("\n");
+                input.mapTo(t -> {
+                    if (isRightEquation2(t.getA().longInteger(), t.getB().toInts())) {
+                        return t.getA().longInteger();
+                    }
 
-            var tuples = lines.mapTo(t -> t.tuple(": "));
-
-            var tuples2 = tuples.mapTo(t -> new Tuple<>(t.getA(), t.getB().split(" ")));
-
-            tuples2.mapTo(t -> {
-                if (isRightEquation2(t.getA().longInteger(), t.getB().toInts())) {
-                    return t.getA().longInteger();
-                }
-
-                return IntInput.zero().asLongInput();
-            }).reduce(LongInput::add).get().solution().print();
+                    return IntInput.zero().asLongInput();
+                }).reduce(LongInput::add).get().solution().print();
+            });
         });
     }
     
     private static boolean isRightEquation(LongInput goal, ListInput.IntListInput nums) {
-        int operator = 0;
-        
-        while (operator < (int) Math.pow(2, nums.size() - 1)) {
-            var bin = Integer.toBinaryString(operator);
-            
-            if (bin.length() < nums.size() - 1) {
-                bin = StringInput.of("0").join(nums.size() - bin.length() - 1).str() + bin;
-            }
-            
-            var binArr = bin.split("");
+        for (var ops : IterTools.product(List.of(0, 1), nums.size() - 1)) {
             var acc = nums.get(0).asLong();
-            
             int i = 1;
-            for (var op : binArr) {
-                if (op.equals("0")) {
+            
+            for (var op : ops) {
+                if (op == 0) {
                     acc *= nums.get(i).asLong();
                 } else {
                     acc += nums.get(i).asLong();
                 }
-                
+
                 i++;
-                
-                if (i - 1 >= binArr.length) {
+
+                if (i - 1 >= ops.size()) {
                     break;
                 }
             }
-            
+
             if (goal.asLong() == acc) {
                 return true;
             }
-            
-            operator++;
         }
         
         return false;
     }
 
     private static boolean isRightEquation2(LongInput goal, ListInput.IntListInput nums) {
-        int operator = 0;
-
-        while (operator < (int) Math.pow(3, nums.size() - 1)) {
-            var repr = getRepr(operator);
-            
-            if (repr.length() < nums.size() - 1) {
-                repr = StringInput.of("0").join(nums.size() - repr.length() - 1).str() + repr;
-            }
-            
-            var binArr = repr.split("");
+        for (var ops : IterTools.product(List.of(0L, 1L, 2L), nums.size() - 1)) {
             var acc = nums.get(0).asLong();
-
             int i = 1;
-            for (var op : binArr) {
-                if (op.equals("0")) {
+            
+            for (var op : ops) {
+                if (op == 0) {
                     acc *= nums.get(i).asLong();
-                } else if (op.equals("1")) {
+                } else if (op == 1) {
                     acc += nums.get(i).asLong();
                 } else {
-                    acc = new StringInput(Long.toString(acc)).concat(nums.get(i).toString()).longInteger().asLong();
+                    var asL = nums.get(i).asLong();
+                    acc = acc * (long) Math.pow(10, (long) (Math.log10(asL)) + 1) + asL;
                 }
 
                 i++;
 
-                if (i - 1 >= binArr.length) {
+                if (i - 1 >= ops.size()) {
                     break;
                 }
             }
@@ -117,27 +101,8 @@ public class Day7 {
             if (goal.asLong() == acc) {
                 return true;
             }
-
-            operator++;
         }
         
         return false;
-    }
-    
-    private static String getRepr(int num) {
-        if (num == 0) {
-            return "0";
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        
-        while (num > 2) {
-            sb.append(num % 3);
-            num /= 3;
-        }
-
-        sb.append(num % 3);
-        
-        return sb.reverse().toString();
     }
 }
