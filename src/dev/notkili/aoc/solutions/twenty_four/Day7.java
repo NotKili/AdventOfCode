d5a267fd2a613e6bef189218ece951429b3499d4
NotKili <familie_kessler.kilian@t-online.de>
2024-12-08 06:27:22 +0100

    feat(2024): introduced caching to Day7 in order to speed up cartesian product calculations

--- src/dev/notkili/aoc/solutions/twenty_four/Day7.java
+++ src/dev/notkili/aoc/solutions/twenty_four/Day7.java
@@ -8,49 +8,59 @@ import dev.notkili.aoc.shared.misc.collections.IterTools;
 import dev.notkili.aoc.shared.misc.collections.list.List;
 import dev.notkili.aoc.shared.parse.InputParser;
 
+import java.util.HashMap;
+
 public class Day7 {
     public static void main(String[] args) {
         System.out.println("Solution 1:");
 
-        TimeTracker.trackTime(() -> {
-            new InputParser(2024, 7).getInput().ifPresent(strInput -> {
-                var input = strInput
-                        .split("\n")
-                        .mapTo(t -> t.tuple(": "))
-                        .mapTo(t -> t.mapB(b -> b.split(" ")));
+        new InputParser(2024, 7).getInput().ifPresent(strInput -> {
+            var input = strInput
+                    .split("\n")
+                    .mapTo(t -> t.tuple(": "))
+                    .mapTo(t -> t.mapB(b -> b.split(" ")));
 
-                input.mapTo(t -> {
-                    if (isRightEquation(t.getA().longInteger(), t.getB().toInts())) {
-                        return t.getA().longInteger();
-                    }
+            input.mapTo(t -> {
+                if (isRightEquation(t.getA().longInteger(), t.getB().toInts())) {
+                    return t.getA().longInteger();
+                }
 
-                    return IntInput.zero().asLongInput();
-                }).reduce(LongInput::add).get().solution().print();
-            });
+                return IntInput.zero().asLongInput();
+            }).reduce(LongInput::add).get().solution().print();
         });
         
         System.out.println("Solution 2:");
-        
-        TimeTracker.trackTime(() -> {
-            new InputParser(2024, 7).getInput().ifPresent(strInput -> {
-                var input = strInput
-                        .split("\n")
-                        .mapTo(t -> t.tuple(": "))
-                        .mapTo(t -> t.mapB(b -> b.split(" ")));
 
-                input.mapTo(t -> {
-                    if (isRightEquation2(t.getA().longInteger(), t.getB().toInts())) {
-                        return t.getA().longInteger();
-                    }
+        new InputParser(2024, 7).getInput().ifPresent(strInput -> {
+            var input = strInput
+                    .split("\n")
+                    .mapTo(t -> t.tuple(": "))
+                    .mapTo(t -> t.mapB(b -> b.split(" ")));
 
-                    return IntInput.zero().asLongInput();
-                }).reduce(LongInput::add).get().solution().print();
-            });
+            input.mapTo(t -> {
+                if (isRightEquation(t.getA().longInteger(), t.getB().toInts())) {
+                    return t.getA().longInteger();
+                }
+
+                if (isRightEquation2(t.getA().longInteger(), t.getB().toInts())) {
+                    return t.getA().longInteger();
+                }
+
+                return IntInput.zero().asLongInput();
+            }).reduce(LongInput::add).get().solution().print();
         });
     }
-    
+
+    private static HashMap<Integer, List<List<Long>>> products1 = new HashMap<>();
+
+    private static List<List<Long>> product1(List<Long> l, int n) {
+        return products1.computeIfAbsent(n, k -> IterTools.product(l, n));
+    }
+
     private static boolean isRightEquation(LongInput goal, ListInput.IntListInput nums) {
-        for (var ops : IterTools.product(List.of(0, 1), nums.size() - 1)) {
+        var goalL = goal.asLong();
+        
+        for (var ops : product1(List.of(0L, 1L), nums.size() - 1)) {
             var acc = nums.get(0).asLong();
             int i = 1;
             
@@ -61,6 +71,10 @@ public class Day7 {
                     acc += nums.get(i).asLong();
                 }
 
+                if (goalL < acc) {
+                    break;
+                }
+
                 i++;
 
                 if (i - 1 >= ops.size()) {
@@ -68,7 +82,7 @@ public class Day7 {
                 }
             }
 
-            if (goal.asLong() == acc) {
+            if (goalL == acc) {
                 return true;
             }
         }
@@ -76,8 +90,16 @@ public class Day7 {
         return false;
     }
 
+    private static HashMap<Integer, List<List<Long>>> products2 = new HashMap<>();
+    
+    private static List<List<Long>> product2(List<Long> l, int n) {
+        return products2.computeIfAbsent(n, k -> IterTools.product(l, n));
+    }
+    
     private static boolean isRightEquation2(LongInput goal, ListInput.IntListInput nums) {
-        for (var ops : IterTools.product(List.of(0L, 1L, 2L), nums.size() - 1)) {
+        var goalL = goal.asLong();
+        
+        for (var ops : product2(List.of(0L, 1L, 2L), nums.size() - 1)) {
             var acc = nums.get(0).asLong();
             int i = 1;
             
@@ -91,6 +113,10 @@ public class Day7 {
                     acc = acc * (long) Math.pow(10, (long) (Math.log10(asL)) + 1) + asL;
                 }
 
+                if (goalL < acc) {
+                    break;
+                }
+
                 i++;
 
                 if (i - 1 >= ops.size()) {
@@ -98,7 +124,7 @@ public class Day7 {
                 }
             }
 
-            if (goal.asLong() == acc) {
+            if (goalL == acc) {
                 return true;
             }
         }
